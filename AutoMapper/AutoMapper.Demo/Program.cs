a67b71126c74d81826734ee2933d0115e002374f
caodaiming <gmawaje@sohu.com>
2014-11-24 13:52:13 +0800

    add automapper code

--- AutoMapper/AutoMapper.Demo/Program.cs
+++ AutoMapper/AutoMapper.Demo/Program.cs
@@ -8,8 +8,19 @@ namespace AutoMapper.Demo
 {
     class Program
     {
+        static Program() {
+            AutoMapper.Mapper.CreateMap<Person,PersonDTO>();
+            AutoMapper.Mapper.CreateMap<PersonDTO,Person>();
+        }
         static void Main(string[] args)
         {
+            Person person = new Person();
+            person.Birthday = DateTime.Now;
+            person.Name = "cao";
+
+            PersonDTO tsource = Mapper.Map<PersonDTO>(person);
+
+            Console.WriteLine("姓名："+tsource.Name+"  "+"出生年月："+tsource.Birthday);
         }
     }
 }
